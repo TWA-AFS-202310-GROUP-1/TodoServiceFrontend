eafaeb3bc13c178918038475999d667c7aa422b7
Xinxin Xu <xxu37@slb.com>
2023-11-10 14:21:13 +0800

    test: add test for getall in todo service

--- src/app/service/todo.service.spec.ts
+++ src/app/service/todo.service.spec.ts
@@ -8,9 +8,30 @@ describe('TodoService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(TodoService);
+    service.items = [
+      {
+        id: 1,
+        title: 'buy milk',
+        description: 'shop',
+        isDone: false,
+      }
+    ]
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get all items when call getAll', () => {
+    const item = service.getAll()
+
+    expect(service.items).toEqual([
+      {
+        id: 1,
+        title: 'buy milk',
+        description: 'shop',
+        isDone: false,
+      }
+    ])
   });
 });
