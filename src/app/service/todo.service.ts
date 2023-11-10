af0452d02004632c303865e6ee46e8252d895241
Tianwei Liu <TLiu25@slb.com>
2023-11-10 13:52:12 +0800

    feat:  todo service create method

--- src/app/service/todo.service.ts
+++ src/app/service/todo.service.ts
@@ -20,8 +20,17 @@ export class TodoService {
     },
   ];
   constructor() { }
-  
+
   getAll() {
     return this.items;
+  }
+
+  create(title :string,description:string){
+    this.items.push({
+      id: this.items.length+1,
+      title: title,
+      description: description,
+      isDone:false
+    })
   }
 }
