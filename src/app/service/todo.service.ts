c3383a1117f8d3c830129d6a0c9467cc7ebbf91b
Xinxin Xu <xxu37@slb.com>
2023-11-10 13:33:09 +0800

    feat: add todo service

---
+++ src/app/service/todo.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { ToDoItem } from 'src/model/ToDoItem';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+  items: ToDoItem[] = [
+    {
+      id: 1,
+      title: 'buy milk',
+      description: 'shop',
+      isDone: false,
+    },
+    {
+      id: 2,
+      title: 'buy bread',
+      description: 'market',
+      isDone: false,
+    }
+  ]
+
+  constructor() { }
+
+  getAll(){
+    return this.items;
+  }
+
+}
