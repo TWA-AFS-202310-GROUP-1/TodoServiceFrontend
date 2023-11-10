de718076616b93ed2a09fe0ccbf71de6b25a89af
Han Xudong <xhan14@slb.com>
2023-11-10 11:30:01 +0800

    feat: add 'to-do-list' component

---
+++ src/app/to-do-list/to-do-list.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ToDoItem } from 'src/model/ToDoItem';
+
+@Component({
+  selector: 'app-to-do-list',
+  templateUrl: './to-do-list.component.html',
+  styleUrls: ['./to-do-list.component.css']
+})
+export class ToDoListComponent {
+  items: ToDoItem[] = [
+    {
+      id: 1,
+      title: 'food',
+      description: 'buy milk',
+      isDone: false
+    },
+    {
+      id: 2,
+      title: 'food',
+      description: 'buy eggs',
+      isDone: false
+    },
+  ]
+}
