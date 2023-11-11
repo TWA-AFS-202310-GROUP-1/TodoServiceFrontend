2f7174c1193388bf9352e5dc08fdbb11f79d03cf
Han Xudong <xhan14@slb.com>
2023-11-11 18:43:59 +0800

    feat: add tests for all method in HTTP service

--- src/app/service/todohttp.service.spec.ts
+++ src/app/service/todohttp.service.spec.ts
@@ -13,13 +13,16 @@ describe('TodohttpService', () => {
   let httpClientSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
-    //TestBed.configureTestingModule({});
-    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
-    //service = TestBed.inject(TodohttpService);
-    service = new TodohttpService(httpClientSpy)
+    httpClientSpy = jasmine.createSpyObj('HttpClient', [
+      'get',
+      'delete',
+      'put',
+      'post',
+    ]);
+    service = new TodohttpService(httpClientSpy);
   });
 
-  it('should get items when call GetAll', () => {
+  it('should get items when call getAll', () => {
     httpClientSpy.get.and.returnValue(
       asyncData([
         {
@@ -31,11 +34,134 @@ describe('TodohttpService', () => {
       ])
     );
 
-    service.getAll().subscribe(data => {
-      expect(data.length).toEqual(1)
+    service.getAll().subscribe((data) => {
+      expect(data.length).toEqual(1);
     });
 
     expect(httpClientSpy.get.calls.count()).toEqual(1);
+  });
 
+  it('should create a new item when call create given title and description', () => {
+    httpClientSpy.post.and.returnValue(
+      asyncData({
+        id: 0,
+        title: 'food',
+        description: 'buy eggs',
+        isDone: false,
+      })
+    );
+
+    service.create('food', 'buy eggs').subscribe((data) => {
+      expect(data).toEqual({
+        id: 0,
+        title: 'food',
+        description: 'buy eggs',
+        isDone: false,
+      });
+    });
+
+    expect(httpClientSpy.post.calls.count()).toEqual(1);
+  });
+
+  it('should mark an item done when call markDone given an item', () => {
+    httpClientSpy.put.and.returnValue(
+      asyncData({
+        id: 0,
+        title: 'food',
+        description: 'buy eggs',
+        isDone: true,
+      })
+    );
+
+    service
+      .markDone({
+        id: 0,
+        title: 'food',
+        description: 'buy eggs',
+        isDone: false,
+      })
+      .subscribe((data) => {
+        expect(data).toEqual({
+          id: 0,
+          title: 'food',
+          description: 'buy eggs',
+          isDone: true,
+        });
+      });
+
+    expect(httpClientSpy.put.calls.count()).toEqual(1);
+  });
+
+  it('should get correct item when call getItemById given id', () => {
+    httpClientSpy.get.and.returnValue(
+      asyncData({
+        id: 0,
+        title: 'food',
+        description: 'buy eggs',
+        isDone: false,
+      })
+    );
+
+    service.getItemById(0).subscribe((data) => {
+      expect(data).toEqual({
+        id: 0,
+        title: 'food',
+        description: 'buy eggs',
+        isDone: false,
+      });
+    });
+
+    expect(httpClientSpy.get.calls.count()).toEqual(1);
+  });
+
+  it('should delete correct item when call deleteItemById given id', () => {
+    httpClientSpy.delete.and.returnValue(
+      asyncData({
+        id: 0,
+        title: 'food',
+        description: 'buy eggs',
+        isDone: false,
+      })
+    );
+
+    service.deleteItemById(0).subscribe((data) => {
+      expect(data).toEqual({
+        id: 0,
+        title: 'food',
+        description: 'buy eggs',
+        isDone: false,
+      });
+    });
+
+    expect(httpClientSpy.delete.calls.count()).toEqual(1);
+  });
+
+  it('should update correct item when call update given id', () => {
+    httpClientSpy.put.and.returnValue(
+      asyncData({
+        id: 0,
+        title: 'food',
+        description: 'buy an apple',
+        isDone: false,
+      })
+    );
+
+    service
+      .update({
+        id: 0,
+        title: 'food',
+        description: 'buy eggs',
+        isDone: false,
+      })
+      .subscribe((data) => {
+        expect(data).toEqual({
+          id: 0,
+          title: 'food',
+          description: 'buy an apple',
+          isDone: false,
+        });
+      });
+
+    expect(httpClientSpy.put.calls.count()).toEqual(1);
   });
 });
