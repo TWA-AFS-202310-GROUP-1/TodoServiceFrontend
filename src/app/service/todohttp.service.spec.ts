303fff092815940035b3965ddc48083dc2ef8e90
Tianwei Liu <TLiu25@slb.com>
2023-11-12 15:08:38 +0800

    test: getbyid

--- src/app/service/todohttp.service.spec.ts
+++ src/app/service/todohttp.service.spec.ts
@@ -12,9 +12,13 @@ describe('TodohttpService', () => {
   let httpClientSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
-    
-    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get','post','put','delete']);
-    service = new TodohttpService(httpClientSpy)
+    httpClientSpy = jasmine.createSpyObj('HttpClient', [
+      'get',
+      'post',
+      'put',
+      'delete',
+    ]);
+    service = new TodohttpService(httpClientSpy);
   });
 
   it('should be created', () => {
@@ -22,57 +26,92 @@ describe('TodohttpService', () => {
   });
 
   it('should get all todo items when call getAll', () => {
-    httpClientSpy.get.and.returnValue(asyncData([
-      {
-        id: 0,
-        title: 'Home work',
-        description: 'Have to complete home work',
-        isDone: false,
-      }
-    ]))
+    httpClientSpy.get.and.returnValue(
+      asyncData([
+        {
+          id: 0,
+          title: 'Home work',
+          description: 'Have to complete home work',
+          isDone: false,
+        },
+      ])
+    );
 
-    service.getAll().subscribe(data =>{
-      expect(data.length).toEqual(1)
-    })
+    service.getAll().subscribe((data) => {
+      expect(data.length).toEqual(1);
+    });
 
-
-    expect(httpClientSpy.get.calls.count()).toEqual(1)
-
+    expect(httpClientSpy.get.calls.count()).toEqual(1);
   });
 
   it('should send post request to create a new todo item', () => {
-    const newTodo = { id: 42,title: 'New Task', description: 'New task description', isDone: false };
+    const newTodo = {
+      id: 42,
+      title: 'New Task',
+      description: 'New task description',
+      isDone: false,
+    };
     httpClientSpy.post.and.returnValue(asyncData(newTodo));
-  
-    service.create(newTodo.title, newTodo.description).subscribe(data => {
+
+    service.create(newTodo.title, newTodo.description).subscribe((data) => {
       expect(data).toEqual(newTodo);
     });
-  
+
     expect(httpClientSpy.post.calls.count()).toEqual(1);
   });
 
   it('should send delete request to remove a todo item', () => {
     const id = 1;
-    httpClientSpy.delete.and.returnValue(asyncData({ id: 1,title: 'New Task', description: 'New task description', isDone: false }));
-  
-    service.delete(id).subscribe(response => {
-      expect(response).toEqual({id: 1, title: 'New Task', description: 'New task description', isDone: false });
+    httpClientSpy.delete.and.returnValue(
+      asyncData({
+        id: 1,
+        title: 'New Task',
+        description: 'New task description',
+        isDone: false,
+      })
+    );
+
+    service.delete(id).subscribe((response) => {
+      expect(response).toEqual({
+        id: 1,
+        title: 'New Task',
+        description: 'New task description',
+        isDone: false,
+      });
     });
-  
+
     expect(httpClientSpy.delete.calls.count()).toEqual(1);
   });
 
   it('should send put request to update a todo item', () => {
-    const updatedTodo = { id: 1, title: 'Updated Task', description: 'Updated task description', isDone: true };
+    const updatedTodo = {
+      id: 1,
+      title: 'Updated Task',
+      description: 'Updated task description',
+      isDone: true,
+    };
     httpClientSpy.put.and.returnValue(asyncData(updatedTodo));
-  
-    service.update(updatedTodo).subscribe(data => {
+
+    service.update(updatedTodo).subscribe((data) => {
       expect(data).toEqual(updatedTodo);
     });
-  
+
     expect(httpClientSpy.put.calls.count()).toEqual(1);
   });
-  
-  
-  
+
+  it('should fetch a single todo item by id', () => {
+    const todoItem = {
+      id: 2,
+      title: 'Specific Task',
+      description: 'Specific task description',
+      isDone: false,
+    };
+    httpClientSpy.get.and.returnValue(asyncData(todoItem));
+
+    service.getItemById(todoItem.id).subscribe((data) => {
+      expect(data).toEqual(todoItem);
+    });
+
+    expect(httpClientSpy.get.calls.count()).toEqual(1);
+  });
 });
--- src/app/service/todohttp.service.ts
+++ src/app/service/todohttp.service.ts
@@ -4,41 +4,42 @@ import { Observable } from 'rxjs';
 import { ToDoItem } from 'src/model/ToDoItem';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodohttpService {
+  constructor(private httpClient: HttpClient) {}
 
+  getAll() {
+    return this.httpClient.get<ToDoItem[]>('https://localhost:44309/ToDoItem');
+  }
 
-  constructor(private httpClient : HttpClient) {
-    
-   }
-
-   getAll() {
-    return this.httpClient.get<ToDoItem[]>('https://localhost:44309/ToDoItem')
-   }
-
-   create(title :string ,description:string) {
-    return this.httpClient.post('https://localhost:44309/ToDoItem',{
-      title:title,
+  create(title: string, description: string) {
+    return this.httpClient.post('https://localhost:44309/ToDoItem', {
+      title: title,
       description: description,
-      isDone:false
-    })
-   }
-   delete(id: number) {
+      isDone: false,
+    });
+  }
+  delete(id: number) {
     return this.httpClient.delete(`https://localhost:44309/ToDoItem/${id}`);
   }
   update(item: ToDoItem) {
-    return this.httpClient.put(`https://localhost:44309/ToDoItem/${item.id}`, item);
+    return this.httpClient.put(
+      `https://localhost:44309/ToDoItem/${item.id}`,
+      item
+    );
   }
-  
+
   getItemById(id: number) {
-    return this.httpClient.get<ToDoItem>(`https://localhost:44309/ToDoItem/${id}`);
+    return this.httpClient.get<ToDoItem>(
+      `https://localhost:44309/ToDoItem/${id}`
+    );
   }
 
   markDone(item: ToDoItem) {
-    return this.httpClient.put(`https://localhost:44309/ToDoItem/${item.id}`, item);
+    return this.httpClient.put(
+      `https://localhost:44309/ToDoItem/${item.id}`,
+      item
+    );
   }
-
-  
-
 }
